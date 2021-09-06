32cb9525d39be2c7c99c7468338104efad873f7b
Luciano Garbarino <lucigar01@gmail.com>
2021-09-06 13:04:13 -0300

    cambios lucho

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-from fastapi import FastAPI, Request, WebSocket
+from fastapi import FastAPI, Request, WebSocket,BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
 
+
 import asyncio
 from time import sleep
 import numpy as np
 from numpy import random
+import uvicorn
 
 from Curvas import crearCsvBadlar, crearCsvCer, crearCsvHardDolar, crearCsvOn
 from Curvas import onlineBadlar, onlineCer, onlineDolar, onlineOn
 from funciones import api_curva_online as api_curva
+
+# Instancias
+#-------------------------------------------------------------------
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+#--------------------------------------------------------------------
+
+#Configuraciones y Servicios
+#--------------------------------------------------------------------
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
@@ -29,14 +38,22 @@ async def websocket_endpoint(websocket: WebSocket):
 # @app.on_event("startup")
 # def startup():
 #     api_curva.main()
-    
+
+def add_log(log):
+    with open('log.txt', 'a') as f:
+        f.write("{}\n".format(log))
+        f.close()
 
 @app.get("/status")
 async def status():
     json ={}
     json["Server Operating"] = True
     return json
-    
+#--------------------------------------------------------------------
+
+# Views
+#--------------------------------------------------------------------
+
 
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
@@ -87,23 +104,28 @@ def actualizar():
     dic["Status On"] = crearCsvOn.main()
     return dic
 
+#--------------------------------------------------------------------
+# ZONA DE PRUEBAS
+#--------------------------------------------------------------------
+# async def fake_video_streamer():
+#     for i in range(10):
+#         aux = random.randint(1, 100)
+#         aux = str(aux)
+#         yield aux+"\n"
 
-# PRUEBA DE STREAMING
 
-async def fake_video_streamer():
-    for i in range(10):
-        aux = random.randint(1, 100)
-        aux = str(aux)
-        yield aux+"\n"
-
-
-@app.get("/test")
-async def test():
-    return StreamingResponse(fake_video_streamer())
+# @app.get("/test")
+# async def test():
+#     return StreamingResponse(fake_video_streamer())
 
 
 # EJEMPLO DE TEMPLATES Y STATICS
 
-# @app.get("/test/{id}", response_class=HTMLResponse)
-# async def test(request: Request, id: str):
-#     return templates.TemplateResponse("test.html", {"request": request, "id": id})
+@app.get("/test/{msg}")
+async def test(request: Request, msg: str):
+    return {"msg":msg}
+
+#--------------------------------------------------------------------
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 start "api_curva" /D funciones api_curva_online.bat
-uvicorn main:app
+main.py
